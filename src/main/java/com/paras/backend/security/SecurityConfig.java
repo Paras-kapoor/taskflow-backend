8280e85f1324ba7266c217d2e7cb2d0d5b60434c
Paras-kapoor <2k22cscys.2212734@gmail.com>
2026-05-02 15:06:08 +0530

    use env variables for Railway

--- src/main/java/com/paras/backend/security/SecurityConfig.java
+++ src/main/java/com/paras/backend/security/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
         org.springframework.web.cors.CorsConfiguration configuration = new org.springframework.web.cors.CorsConfiguration();
-        configuration.setAllowedOrigins(java.util.List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(java.util.List.of("http://localhost:5173","https://taskflow-backend-production-cd70.up.railway.app"));
         configuration.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(java.util.List.of("*"));
         configuration.setAllowCredentials(true);
